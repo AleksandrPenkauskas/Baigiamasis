9a9f901ca146836487f4d2397f9059dac8610443
Aleks <aleksandr.penkauskas@gmail.com>
2023-05-04 16:37:16 +0300

    Begining

---
+++ src/main/java/lt/aleksandr/baigiamasis/BaigiamasisApplication.java
@@ -0,0 +1,13 @@
+package lt.aleksandr.baigiamasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaigiamasisApplication.class, args);
+	}
+
+}
